1d3e64f736e0547033c5d7feb338c8c7968595b6
RestCode <stamtsag5@gmail.com>
2022-11-14 15:00:28 +0200

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# Discord Leveling bot in Python
---
+++ leveling_bot/leveling_bot.py
@@ -0,0 +1,8 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+client.run('TOKEN')
