f5ac73d42422ea141aaa13a7dc2d5966b903578b
RestCode <stamtsag5@gmail.com>
2022-12-03 19:51:02 +0200

    Add `dotenv`

--- leveling-bot/leveling-bot.py
+++ leveling-bot/leveling-bot.py
@@ -1,9 +1,12 @@
 import discord
 from discord.ext.commands import Bot
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = Bot(command_prefix='R', intents=intents)
 
-bot.run('TOKEN')
+bot.run(os.getenv('TOKEN'))
